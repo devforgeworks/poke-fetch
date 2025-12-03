92978d299159ae276e6315407782665168be65ef
devforgeworks <devforgeworks@gmail.com>
2025-12-03 22:43:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,91 +1,85 @@
-const clearButton = document.getElementById("clearBtn");
-const fetchBtn = document.getElementById("fetchBtn");
+document.getElementById("Fbtn").addEventListener("click", FetchData);
+document.getElementById("Sbtn").addEventListener("click", fetchShiny);
+document.getElementById("Dbtn").addEventListener("click", fetchDream);
+
+async function FetchData(){
+
+    try{
+        const PokeInput = document.getElementById("search").value.toLowerCase();
+        const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${PokeInput}`);
+
+        if(!response.ok){
+            throw new Error("Could not find Pokémon");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        const PokeName = document.getElementById("PokeName");
+        const PokeImg = document.getElementById("PokeImg");
+        const PokeId = document.getElementById("PokeId");
+        const PokeH = document.getElementById("PokeH");
+        const PokeW = document.getElementById("PokeW");
 
 
-fetchBtn.addEventListener("click", () => btnHandler(1));
-shinyBtn.addEventListener("click", () => btnHandler(2));
-clearButton.addEventListener("click", () => btnHandler(3));
-
-
-async function fetchData(){
-
-    try {
-        const pokemonName = document.getElementById("pokeName").value.toLowerCase();
-        const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        const shinyBtn = document.getElementById("shinyBtn");
-        
-
-    if(!response.ok){
-        throw new Error("Could not find pokemon");
-    }
-
-    const data = await response.json();
-    const pokemonSprite = data.sprites.front_default;
-    const nameOfPoke = data.name;
-    const idOfPoke = data.id;
-
-    const id = document.getElementById("idOfPoke");
-    const name = document.getElementById("nameOfPoke");
-    const imgElement = document.getElementById("pokemonSprite");
-
-    console.log(data);
-
-    imgElement.src = pokemonSprite;
-    name.innerText = nameOfPoke;
-    id.innerText = idOfPoke;
-
-    imgElement.style.display="block";
-    shinyBtn.style.display="block"
-    name.style.display="block";
-    id.style.display="block";
+        PokeName.innerText = data.name.toUpperCase();
+        PokeImg.src = data.sprites.front_default;
+        PokeId.innerText = data.id;
+        PokeH.innerText = data.height;
+        PokeW.innerText = data.weight;
 
     }
 
     catch (error){
-        console.log(error);
-        alert("Colud not find Pokemon");
+        console.log("Error");
     }
+
+
 }
 
 async function fetchShiny(){
-    const pokemonName = document.getElementById("pokeName").value.toLowerCase();
-    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    const data = await response.json();
-
-    const imgElement = document.getElementById("pokemonSprite");
-    const shinySprite = data.sprites.front_shiny;
-
-    imgElement.src = shinySprite;
-}
-
-function clearInput(){
-    const pokemonName = document.getElementById("pokeName");
-    const imgElement = document.getElementById("pokemonSprite");
-    const id = document.getElementById("idOfPoke");
-    const name = document.getElementById("nameOfPoke");
-
-    imgElement.style.display="none";
-    shinyBtn.style.display="none"
-    name.style.display="none";
-    id.style.display="none";
-
-
-    pokemonName.value="";
-    pokemonName.focus();
-
     
+    const PokeInput = document.getElementById("search").value.toLowerCase();
+    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${PokeInput}`);
+    const data  = await response.json();
+
+    const PokeImg = document.getElementById("PokeImg");
+    PokeImg.src = data.sprites.front_shiny;
+
+          const PokeName = document.getElementById("PokeName");
+        
+        const PokeId = document.getElementById("PokeId");
+        const PokeH = document.getElementById("PokeH");
+        const PokeW = document.getElementById("PokeW");
+
+
+        PokeName.innerText = data.name.toUpperCase();
+        PokeImg.src = data.sprites.front_default;
+        PokeId.innerText = data.id;
+        PokeH.innerText = data.height;
+        PokeW.innerText = data.weight;
+
 }
 
-function btnHandler(btnNumber){
+async function fetchDream(){
+    const PokeInput = document.getElementById("search").value.toLowerCase();
+    const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${PokeInput}`);
+    const data  = await response.json();
 
-    if(btnNumber === 1){
-        fetchData();
-    } else if(btnNumber === 2){
-        fetchShiny();
-    }else if(btnNumber === 3){
-        clearInput();
-    }
+    const PokeImg = document.getElementById("PokeImg");
+    PokeImg.src = data.sprites.other.dream_world.front_default;
+
+          const PokeName = document.getElementById("PokeName");
+        
+        const PokeId = document.getElementById("PokeId");
+        const PokeH = document.getElementById("PokeH");
+        const PokeW = document.getElementById("PokeW");
+
+
+        PokeName.innerText = data.name.toUpperCase();
+        PokeImg.src = data.sprites.front_default;
+        PokeId.innerText = data.id;
+        PokeH.innerText = data.height;
+        PokeW.innerText = data.weight;
 
 }
-
-
